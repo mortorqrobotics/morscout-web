291ef3d2271ed4a94a301d8c593680cc4048c5be
thatarvinguy <zadeharvin@gmail.com>
2016-02-02 18:02:01 -0800

    Adds scouting form value gathering and scouting form on report page

--- js/main.js
+++ js/main.js
@@ -103,3 +103,226 @@ $("#logoutButton").on("click", function(){
 		}
 	});
 });
+
+function synthesizeForm(dataPoints) {
+	$('#form-preview').empty();
+	var form = $('#form-preview');
+	var oneColumn = true;
+	for (var i = 0; i < dataPoints.length; i++) {
+		var dataPoint = dataPoints[i];
+		var dpName = dataPoint.name;
+		var dpType = dataPoint.type;
+		if (dpType == "radio") {
+			var options = dataPoint.options;
+			var div = $(document.createElement("div"));
+			div.addClass("radio inp");
+			div.attr("data-dpName", dpName);
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			for (var j = 0; j < options.length; j++) {
+				var option = options[j];
+				var input = $(document.createElement("input"));
+				input.attr("type", "radio");
+
+				input.attr("name", dpName);
+				input.attr("id", option);
+				input.addClass("report-check");
+				var label = $(document.createElement("label"));
+				label.addClass("report-text report-radio-check");
+				label.attr("for", option);
+				label.html(option);
+				div.append(input);
+				div.append(label);
+				div.append("<br/>");
+			}
+			var p = $(document.createElement("p"));
+			p.addClass("report-text");
+			p.html(dpName);
+			form.append(div);
+
+		} else if (dpType == "dropdown") {
+			var div = $(document.createElement('div'));
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			div.addClass("dropdown inp");
+			var options = dataPoint.options;
+			var select = $(document.createElement("select"));
+			select.attr("id", dpName);
+			select.addClass("creator-dropdown");
+			for (var j = 0; j < options.length; j++) {
+				var optionText = options[j];
+				var option = $(document.createElement("option"));
+				option.html(optionText);
+				select.append(option);
+			}
+			var p = $(document.createElement("p"));
+			p.addClass("report-text");
+			p.html(dpName + ": ");
+			div.append(p);
+			div.append(select);
+			form.append(div)
+		} else if (dpType == "number") {
+			var div = $(document.createElement('div'));
+			div.addClass("number inp");
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			var start = dataPoint.start;
+			var min = dataPoint.min;
+			var max = dataPoint.max;
+
+			var p = $(document.createElement("p"));
+			p.addClass("report-text");
+			p.html(dpName + ": ");
+
+			var inputBox = $(document.createElement("input"));
+			inputBox.attr("type", "number");
+			inputBox.addClass("report-box-small inp-val-box");
+			inputBox.attr("id", dpName);
+			inputBox.attr("min", min);
+			inputBox.attr("max", max);
+			inputBox.val(start);
+			inputBox.keyup(function(){
+				if (parseInt(inputBox.val()) < min || inputBox.val().indexOf(".") > -1){
+					inputBox.val(min);
+				}
+				else if (parseInt(inputBox.val()) > max){
+					inputBox.val(max);
+				}
+			});
+
+			var inputSubtract = $(document.createElement("input"));
+			inputSubtract.attr("type", "button");
+			inputSubtract.addClass("button add-minus-button");
+			inputSubtract.attr("id", dpName);
+			inputSubtract.val("-");
+			inputSubtract.click(function(){
+				if (parseInt(inputBox.val()) > min){
+					inputBox.val(parseInt(inputBox.val()) - 1);
+				}
+			});
+
+			var inputAdd = $(document.createElement("input"));
+			inputAdd.attr("type", "button");
+			inputAdd.attr("id", dpName);
+			inputAdd.addClass("button add-minus-button");
+			inputAdd.val("+");
+			inputAdd.click(function(){
+				if (parseInt(inputBox.val()) < max){
+					inputBox.val(parseInt(inputBox.val()) + 1);
+				}
+			});
+
+			div.append(p);
+			div.append(inputSubtract);
+			div.append(inputBox);
+			div.append(inputAdd);
+			form.append(div);
+		} else if (dpType == "checkbox") {
+			var div = $(document.createElement('div'));
+			div.addClass("checkbox inp");
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			var input = $(document.createElement("input"));
+			input.attr("type", "checkbox");
+			input.addClass("report-check");
+			input.attr("id", dpName);
+			var label = $(document.createElement("label"));
+			label.attr("for", dpName);
+			label.addClass("report-radio-check report-text");
+			label.html(dpName);
+			div.append(input);
+			div.append(label);
+			form.append(div);
+		} else if (dpType == "label") {
+			var div = $(document.createElement('div'));
+			div.addClass("label inp");
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			var h3 = $(document.createElement("h3"));
+			h3.addClass("scout-div-title");
+			h3.html(dpName);
+			div.append(h3);
+			form.append(div);
+		} else if (dpType == "text") {
+			var div = $(document.createElement('div'));
+			div.addClass("text inp");
+			// if (oneColumn == true) {
+			//   div.addClass('col-md-12');
+			//   oneColumn = false;
+			// }
+			// else {
+			//   div.addClass('col-md-6');
+			// }
+			var textarea = $(document.createElement("textarea"));
+			textarea.addClass("report-area");
+			textarea.attr("placeholder", dpName);
+			div.append(textarea);
+			form.append(div);
+		}
+		$('#form-preview').append("</br></br>");
+	}
+}
+
+
+function getScoutFormValues(){
+	var form = $("#form-preview");
+	var inputs = form.find(".inp");
+	var values = {};
+	for (var i = 0; i < inputs.length; i++){
+		var inp = $(inputs[i]);
+		if (inp.hasClass("number")){
+			var dpName = inp.find(".inp-val-box").eq(0).attr("id");
+			var value = parseInt(inp.find(".inp-val-box").eq(0).val());
+			values[dpName] = value;
+		}
+		else if (inp.hasClass("text")){
+			var dpName = inp.find(".report-area").eq(0).attr("placeholder");
+			var value = inp.find(".report-area").eq(0).val();
+			values[dpName] = value;
+		}
+		else if (inp.hasClass("label")){
+			//do nothing?
+		}
+		else if (inp.hasClass("checkbox")){
+			var dpName = inp.find("input").eq(0).attr("id");
+			var value = inp.find("input").prop("checked");
+			values[dpName] = value;
+		}
+		else if (inp.hasClass("radio")){
+			var dpName = inp.attr("data-dpName");
+			var value = $('.report-check[name="'+dpName+'"]:checked').attr("id");//eh but works
+			values[dpName] = value;
+		}
+		else if (inp.hasClass("dropdown")){
+			var dpName = inp.find(".creator-dropdown").eq(0).attr("id");
+			var value = inp.find(".creator-dropdown").eq(0).val();
+			values[dpName] = value;
+		}
+	}
+	//values is the complete object of form values
+}
